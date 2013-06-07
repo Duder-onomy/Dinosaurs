5729c577d2832cb92d88b438100ba410e79ceda6
Duder-Onomy <greglarrenaga@gmail.com>
2013-06-07 15:06:00 -0700

    Add Auto Run Feature!!

--- AsciiArt.rb
+++ AsciiArt.rb
@@ -1,15 +1,22 @@
 class Dinosaur
 
   def self.run
+    puts "Welcome to Dino Ascii Art!"
     #call self.get_dinosaur_files
     get_dinosaur_files
+
     loop do
       #call self.list
       list
-      puts "Would you like more dino's? [y]es [n]o"
+
+      puts "What Dinosaur do you want to select? (by index)"
+      input = gets.chomp.to_i
+      show(@file_names[input])
+
+      puts "Would you like more dinos? [y]es [n]o"
       input = gets.chomp
       if input == 'n'
-        return puts "GoodBye"
+        return puts "Goodbye"
       end
     end
   end
@@ -28,45 +35,24 @@ class Dinosaur
       file_name = file_name.gsub(/^Dinosaurs\//, "").gsub(/.txt$/, "")
       puts "[#{index}] - #{file_name}" 
     end
-    puts "What Dinosaur do you want to select? (by index)"
-    input = gets.chomp.to_i
-    show(@file_names[input])
   end
 
   def self.show(dinosaur_name)
-    # #call file lookup to get dinosaurs file name
-    # file_name = file_lookup(dinosaurname)
-    # #call open_file to open and get results 
-    dino = open_file(dinosaur_name)
-    #put the on screen
-    puts dino.read
-  end
-
-  def self.file_lookup(dinosaur_name)
-    #match dinosaur name to dinosaur file
-    return 'Dinosaur1.txt'
-    
-    file_names = []
-
-    files = Dir.glob("Dinosaurs/*.txt")
-    files.each_with_index do |file, index|
-      file_names[index] = file
-    end
+    #call open_file to open and get results 
+    puts open_file(dinosaur_name).read
   end
 
   def self.open_file(file_name)
     #open file, return results
-    dino = File.open file_name, 'r'
+    File.open file_name, 'r'
   end
 
 
 
-
-  #automatiacally look in folder for all dinosaurs. 
-  #dinosaur.help Will list out the names of the files without extension. nut get that data recursively
   #command line args
-  #Next iteration will print a speech bubble
+  #When Loading File, print a speech bubble Dino With Hello and current time.
   #Final Iteration will print any word in ASCII Chars.
   #make other beasts
-
 end
+
+Dinosaur.run
