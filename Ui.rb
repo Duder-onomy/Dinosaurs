1e06f723477d735822aa3abac26f047c04ce822d
Dustin Zeisler <dustin@zive.me>
2013-06-08 20:49:48 -0700

    Add color to art work, is displayed randomly at each picture selection

--- Ui.rb
+++ Ui.rb
@@ -1,6 +1,15 @@
-class Ui 
-  def self.display(string)
-    puts string
+require 'term/ansicolor'
+class Color
+  extend Term::ANSIColor
+  def self.colors
+    [:black,:red, :green,:yellow,:blue,:magenta,:cyan,:white].sample
+  end
+end
+
+
+class Ui
+  def self.display(string, color=Color.colors)
+    puts Color.send(color), string, Color.clear, "\n"
   end
   def self.ask(question)
     puts question
@@ -8,14 +17,14 @@ class Ui
   end
   def self.ask_which(names, question)
     selected_name = nil
-    while selected_name.nil? 
-      display (question)
+    while selected_name.nil?
+      display(question)
       names.each_with_index do |name, index|
         puts "[#{index}] - #{name}"
       end
       input = gets.chomp.to_i
       selected_name =  names.at(input)
-    end 
+    end
     return selected_name
   end
 end
